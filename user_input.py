7a7c10b47a0d0b12e5f2c77f5196709fe78e02d1
Smoruri9 <152043330+Smoruri9@users.noreply.github.com>
2024-02-19 21:46:31 +0900

    Files via upload
    User_input file for week 1 python assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name ")
+age = input("Enter age ")
+location = input("Where do you live? ")
+
+print("Hello {}, you are {} years old and live in {}".format(name, age, location))
